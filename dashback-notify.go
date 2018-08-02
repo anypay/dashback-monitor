c29fd77a15c01868ffab8438cce7a6217fb86bf5
Steven Zeiler <me@stevezeiler.com>
2018-08-02 18:29:06 -0400

    Properly parse int64 into float64, string

--- dashback-notify.go
+++ dashback-notify.go
@@ -12,6 +12,9 @@ import (
 )
 
 func main() {
+
+  CheckBalance();
+
 	s := gocron.NewScheduler()
 	s.Every(1).Hours().Do(CheckBalance)
 	<-s.Start()
@@ -22,11 +25,11 @@ func CheckBalance() {
 
 	// Connect to local dash RPC server using HTTP POST mode.
 	connCfg := &rpcclient.ConnConfig{
-		Host:         "localhost:9998",
+		Host:         "dash.batm.anypay.global:443",
 		User:         "dash",
 		Pass:         os.Getenv("DASH_PASS"),
 		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
-		DisableTLS:   true, // Bitcoin core does not provide TLS by default
+		DisableTLS:   false, // Bitcoin core does not provide TLS by default
 	}
 	// Notice the notification parameter is nil since notifications are
 	// not supported in HTTP POST mode.
@@ -43,41 +46,26 @@ func CheckBalance() {
 	}
 	log.Printf("Wallet balance: %d", WalletBalance)
 
-	if WalletBalance < 1 {
-		webhookUrl := "https://hooks.slack.com/services/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"
+  balance := float64(WalletBalance) / 100000000
 
-		//	attachment1 := slack.Attachment{}
-		//	attachment1.AddField(slack.Field{Title: "Balance", Value: Balance})
-		payload := slack.Payload{
-			Text:      "Dashback wallet balance is below threshold!",
-			Username:  "dashback-notifier",
-			Channel:   "#dashback-notify",
-			IconEmoji: ":warning:",
-			//		Attachments: []slack.Attachment{attachment1},
-		}
-		err := slack.Send(webhookUrl, "", payload)
-		if len(err) > 0 {
-			fmt.Printf("error: %s\n", err)
-		}
-	}
+  webhookUrl := "https://hooks.slack.com/services/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"
+
+
+  if (balance > 0) {
+    //	attachment1 := slack.Attachment{}
+    //	attachment1.AddField(slack.Field{Title: "Balance", Value: Balance})
+    payload := slack.Payload{
+      Text: fmt.Sprintf("Dashback wallet balance is (%f).", balance),
+      Username:  "dashback-notifier",
+      Channel:   "#dashback-notify",
+      IconEmoji: ":warning:",
+      //		Attachments: []slack.Attachment{attachment1},
+    }
+
+    err := slack.Send(webhookUrl, "", payload)
+    if len(err) > 0 {
+      fmt.Printf("error: %s\n", err)
+    }
+  }
 }
 
-/*
-func SlackAlert() {
-	webhookUrl := "https://hooks.slack.com/services/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"
-
-	attachment1 := slack.Attachment{}
-	attachment1.AddField(slack.Field{Title: "Balance", Value: "0.00000000"})
-	payload := slack.Payload{
-		Text:        "Dashback wallet balance is below threshold!",
-		Username:    "dashback-notifier",
-		Channel:     "#dashback-notify",
-		IconEmoji:   ":warning:",
-		Attachments: []slack.Attachment{attachment1},
-	}
-	err := slack.Send(webhookUrl, "", payload)
-	if len(err) > 0 {
-		fmt.Printf("error: %s\n", err)
-	}
-}
-*/
